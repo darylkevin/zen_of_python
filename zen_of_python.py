b32425161f7a36fc2d79f71942f34a800fa676cf
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-08-04 20:12:05 +0800

    Feat: Define function zop

--- zen_of_python.py
+++ zen_of_python.py
@@ -5,3 +5,20 @@ from collections import Counter
  pattern = "[A-Z]{3}[\s]{1}[A-Z][\w\d\s\\',\-\*]*[.\!]"
  word_dict = {}
 
+def zen_of_python(file):
+     with open(file) as inputfile:
+         content = inputfile.read()
+         match = re.findall(pattern, content)
+         for matches in match:
+             clean_lines = matches[4:]
+             print(clean_lines)
+         for matches in match:
+             clean_lines = matches[4:]
+             words = clean_lines.split(' ')
+             for word in words:
+                 word = word.lower()
+                 if word in word_dict:
+                     word_dict[word] = word_dict[word] + 1
+                 else:
+                     word_dict[word] = 1
+
