39d2246a7e41ac08ba05187857165fb24184eea2
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-08-05 20:20:38 +0800

    Docs: Add comments

--- zen_of_python.py
+++ zen_of_python.py
@@ -9,19 +9,26 @@ word_dict = {}
 def zen_of_python(file):
     with open(file) as inputfile:
         content = inputfile.read()
+        # Finds ZOP from file using regular expression
         match = re.findall(pattern, content)
+        # Prints out ZOP lines without the ZOP marker
         for matches in match:
             clean_lines = matches[4:]
             print(clean_lines)
+        # Splits lines of ZOP for our word count 
         for matches in match:
             clean_lines = matches[4:]
             words = clean_lines.split(' ')
+            # Counts words in ZOP and eliminates words with
+            # different cases to be not considered the same word
             for word in words:
                 word = word.lower()
                 if word in word_dict:
                     word_dict[word] = word_dict[word] + 1
                 else:
                     word_dict[word] = 1
+    # Sorts out words by how frequently they are used
+    # and returns the 5 most common words
     occurrence_count = Counter(word_dict)
     top_words = occurrence_count.most_common(5)
     print('\nTOP 5 WORDS\n'
